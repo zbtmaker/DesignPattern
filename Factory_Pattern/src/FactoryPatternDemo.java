f0776bbacfea9d04f9fc654ff02cceeb7a1fefb2
zbtmaker <33961921+zbtmaker@users.noreply.github.com>
2018-04-20 17:52:02 +0800

    Update FactoryPatternDemo.java

--- Factory_Pattern/src/FactoryPatternDemo.java
+++ Factory_Pattern/src/FactoryPatternDemo.java
@@ -3,15 +3,15 @@ package Factory_Pattern;
 public class FactoryPatternDemo {
 	public static void main(String[] args) {
 		ShapeFactory sf = new ShapeFactory();
-		//
+		//get a Circle Object
 		Shape s1 = sf.getShape("Circle");
 		s1.draw();
 		
-		//
+		//get a Square Object
 		Shape s2 = sf.getShape("Square");
 		s2.draw();
 		
-		//
+		//get a Rectangle Object
 		Shape s3 = sf.getShape("Rectangle");
 		s3.draw();
 	}
