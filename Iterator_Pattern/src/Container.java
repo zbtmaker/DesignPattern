a43ee99bf41bb6286e7251984320e86f8540454b
zbtmaker <33961921+zbtmaker@users.noreply.github.com>
2018-04-20 22:30:17 +0800

    Add files via upload

---
+++ Iterator_Pattern/src/Container.java
@@ -0,0 +1,5 @@
+package Iterator_Pattern;
+
+public interface Container {
+	Iterator getIterator();
+}
---
+++ Iterator_Pattern/src/Iterator.java
@@ -0,0 +1,6 @@
+package Iterator_Pattern;
+
+public interface Iterator {
+	boolean hasNext();
+	Object next();
+}
